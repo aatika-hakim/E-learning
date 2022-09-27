6dbfef270fe04c24799107aa705075f70840a2ea
Aatika Hakim <aatikahakim90@gmail.com>
2022-09-27 11:18:03 +0500

    added

---
+++ src/components/Layout.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <>
+      <div className="empty"></div>
+      <section id="banner">
+        {/* <img /> */}
+        <div className="banner-text">
+          <h1>Learn Quran</h1>
+          <p>my web</p>
+          <div className="banner-btn">
+            <a href="#">
+              <span ></span>Find Out
+            </a>
+            <a href="#">
+              <span ></span>Read More
+            </a>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+export default Layout
