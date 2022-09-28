618ed89a27379324a731fe7f789889e855e84fa4
Aatika Hakim <aatikahakim90@gmail.com>
2022-09-28 13:25:35 +0500

    inline css text color property

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,13 +3,14 @@ import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
 
 const NavBar = () => {
     return (
-        <Navbar bg="light" expand="lg">
-            <Container>
+        <>
+        <Navbar className='nav' expand="lg">
+            <Container className=''>
                 <Navbar.Brand href="#home"></Navbar.Brand>
-                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
+                <Navbar.Toggle className='bg-white'/>
+                <Navbar.Collapse >
                     <Nav className="me-auto">
-                         <Nav.Link href="#home">HOME</Nav.Link>
+                        <Nav.Link href="#home" className='text-white'>HOME</Nav.Link>
                         <NavDropdown title="COURSES" id="basic-nav-dropdown">
                         <div>
                             <NavDropdown.Item className='bg-black text-white' href="#">Action</NavDropdown.Item>
@@ -17,11 +18,12 @@ const NavBar = () => {
                             <NavDropdown.Item className='bg-black text-white' href="#">Something</NavDropdown.Item>
                         </div>
                         </NavDropdown>
-                        <Nav.Link href="#home">FEE</Nav.Link>
+                        <Nav.Link href="#home" className='text-white'>FEE</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
         </Navbar>
+        </>
     );
 }
 
